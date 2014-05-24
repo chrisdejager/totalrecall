9b98400c26754716fac9258bc85da00d253c860a
Chris de Jager <email@chrisdejager.nl>
2014-05-24 12:49:24 +0200

    path to buffer_bci now command line option

--- replay/startOsc.sh
+++ replay/startOsc.sh
@@ -1,8 +1,15 @@
 #! /usr/bin/env bash
-cd buffer_bci/dataAcq/ 
+
+if [ -z $1 ]
+then
+  echo Provide path to buffer_bci *with* trailing slash
+  exit 1
+fi
+
+cd $1dataAcq/
 
 oscport=9090
 oscdevice=92E6
 # 1) run the OSC -> ft_buffer converter with parameters for the MUSE  !in the background!
 #    This will then wait for data from the MUSE and connection to the buffer
-java -cp buffer/java/Buffer.jar:osc/JavaOSC.jar:osc osc2ft /muse/eeg/raw:${oscport} localhost:1972 4 500 1 10 &
+java -cp buffer/java/Buffer.jar:osc/JavaOSC.jar:osc osc2ft /muse/eeg/raw:${oscport} localhost:1972 4 500 1 10
